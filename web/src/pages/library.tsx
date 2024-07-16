fada88ff47ec4143b3bb5e4570ba1a54cb01fe24
Ahn Ming Loke <lokeahnming@gmail.com>
2024-07-15 23:44:25 -0400

    feat: add react-router-dom, set up routing scaffold

---
+++ web/src/pages/library.tsx
@@ -0,0 +1,10 @@
+
+const Library = () => {
+  return (
+    <div className="bk_lib">
+      <h1>Library</h1>
+    </div>
+  )
+}
+
+export default Library;
---
+++ web/src/pages/login.tsx
@@ -0,0 +1,13 @@
+
+const Login = () => {
+  return (
+    <div className="bk_login">
+      <h1>Login</h1>
+        <form>
+          <button>Sign in with Google</button>
+        </form>
+    </div>
+  )
+}
+
+export default Login;
