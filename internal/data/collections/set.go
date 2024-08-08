130752c12a10a6e7dff64e43d6356f17a24540f6
Ahn Ming Loke <lokeahnming@gmail.com>
2024-08-08 19:38:24 -0400

    feat: add custom Set data structure for use across models and handlers

---
+++ internal/data/collections/set.go
@@ -0,0 +1,39 @@
+package collections
+
+var exists = struct{}{}
+
+// HashSet represents a collection of unique strings
+type Set struct{
+	m map[string]struct{}
+}
+
+// Creates and Returns a new Set
+func NewSet() *Set {
+	return &Set{m:make(map[string]struct{})}
+}
+
+// Inserts a new element into Set
+func (s *Set) Add(value string) {
+	s.m[value] = exists
+}
+
+// Deletes an element from Set
+func (s *Set) Delete(value string) {
+	delete(s.m, value)
+}
+
+// Checks if an element exists in Set
+func (s *Set) Has(value string) bool {
+	_, exists := s.m[value]
+	return exists
+}
+
+// Returns the number of elements in Set
+func (s *Set) Size() int {
+	return len(s.m)
+}
+
+// Removes all elements from Set
+func (s *Set) Clear() {
+	s.m = make(map[string]struct{})
+}
