24e4db15da496d38180386fd7247c47412f246f3
Ahn Ming Loke <lokeahnming@gmail.com>
2024-11-19 13:57:35 -0500

    fix: adjust formatPublishDate helper for BookForm

--- web/src/utils/formatPublishDate.ts
+++ web/src/utils/formatPublishDate.ts
@@ -14,20 +14,20 @@ export const useFormatPublishDate = (publishDate: string | undefined): FormatPub
 
     if (!publishDate) {
       const currentYear = new Date().getFullYear();
-      warning = "Google Books didn't give us a valid publish date. Please double check the publish date.";
+      warning = "Google Books didn't give us a valid publish date. Please double-check this book's publish date.";
       formattedDate = `${currentYear}-01-01`;
     } else {
       const dateRegex = /^\d{4}(-\d{2}(-\d{2})?)?$/;
       if (!dateRegex.test(publishDate)) {
-        warning = "Google Books didn't give us a valid publish date. Please double check the publish date.";
+        warning = "Google Books didn't give us a valid publish date. Please double-check this book's publish date.";
         formattedDate = `${new Date().getFullYear()}-01-01`;
       } else {
         const parts = publishDate.split('-');
         if (parts.length === 1) {
-          warning = "Google Books only gave us the publish year. Please double check this book's publish date.";
+          warning = "Google Books only gave us the publish year. Please double-check this book's publish date.";
           formattedDate = `${parts[0]}-01-01`;
         } else if (parts.length === 2) {
-          warning = "Google Books only gave us the publish year and month. Please double check this book's publish date.";
+          warning = "Google Books only gave us the publish year and month. Please double-check this book's publish date.";
           formattedDate = `${parts[0]}-${parts[1]}-01`;
         } else {
           formattedDate = publishDate;
